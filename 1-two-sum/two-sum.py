4be5f7a6df36d415692e33575710471b51ea535c
William Sukjaem <133365639+Willsu00@users.noreply.github.com>
2025-09-05 17:37:49 +1200

    Time: 4 ms (53.64%) | Memory: 13.1 MB (77.33%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -7,19 +7,20 @@ class Solution(object):
         """
         # O(n) Solution
 
-        # map = {}
+        map = {}
 
-        # for i, n in enumerate(nums):
-        #     d = target - n
-        #     if d in map:
-        #         return [map[d], i]
-        #     map[n] = i
+        for i, n in enumerate(nums):
+            d = target - n
+            if d in map:
+                return [map[d], i]
+            map[n] = i
 
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                x = nums[i] + nums[j]
-                if x == target:
-                    return i, j
+        # O(n^2) initial solution
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         x = nums[i] + nums[j]
+        #         if x == target:
+        #             return i, j
 
 
         
