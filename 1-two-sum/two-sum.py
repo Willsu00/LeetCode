1e7dec3f3828a6d51abb33f675b88f4c1d596330
William Sukjaem <133365639+Willsu00@users.noreply.github.com>
2026-06-14 17:13:01 +1200

    Time: 2189 ms (13.58%) | Memory: 13.2 MB (35.93%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,22 +5,8 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        # O(n) Solution
-
-        map = {}
-
-        for i, n in enumerate(nums):
-            d = target - n
-            if d in map:
-                return [map[d], i]
-            map[n] = i
-
-        # O(n^2) initial solution
-        # for i in range(len(nums)):
-        #     for j in range(i+1, len(nums)):
-        #         x = nums[i] + nums[j]
-        #         if x == target:
-        #             return i, j
-
-
         
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i]+nums[j]==target:
+                    return i, j
