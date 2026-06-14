8ccdf954319e4144fe0272e2dc766a4b72325843
William Sukjaem <133365639+Willsu00@users.noreply.github.com>
2026-06-14 23:22:09 +1200

    Time: 0 ms (100.00%) | Memory: 10.9 MB (13.90%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -5,13 +5,10 @@ public:
     bool isPalindrome(int x) {
         string s = to_string(x);
         string st = s;
-        
         reverse(st.begin(), st.end());
-
         if (s == st) {
             return true;
         }
-        return false;
-
+        return false;    
     }
 };
