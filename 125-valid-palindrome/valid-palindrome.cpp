d7d1288399d1c2fe58caaf6992661a84181ccf2d
William Sukjaem <133365639+Willsu00@users.noreply.github.com>
2026-06-15 19:05:07 +1200

    Time: 1 ms (43.70%) | Memory: 10.7 MB (8.25%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -4,25 +4,16 @@ class Solution {
 public:
     bool isPalindrome(string s) {
         string str = "";
-        
-
         for (int i = 0; i < s.size(); i++){
             if (isalnum(s[i])){
                 str += tolower(s[i]);
             }
         }
-
         string st = str;
         reverse(str.begin(), str.end());
         if (str == st){
             return true;
         }
-
-        // cout << str;
-
-        return 0;
-
-
-
+       return 0;
     }
 };
