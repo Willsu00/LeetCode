632cd535a05c7400e4773b488f9f854daf373801
William Sukjaem <133365639+Willsu00@users.noreply.github.com>
2026-06-21 16:33:05 +1200

    Time: 0 ms (100.00%) | Memory: 11.7 MB (26.67%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -1,38 +1,16 @@
-// class Solution {
-// public:
-//     vector<int> plusOne(vector<int>& digits) {
-//         int n = digits.size() - 1;
-//         digits[n]++;
-//         for (int i = n; i >= 0; i--) {
-//             if (digits[i] == 10 && i-1 < 0) {
-//                 digits[i] = 0;
-//                 digits.insert(digits.begin(), 1);
-//             }
-//             if (digits[i] == 10) {
-//                 digits[i-1]++;
-//                 digits[i] = 0;
-//             }
-//         }
-//         return digits;
-//     }
-// };
-
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        int n = digits.size();
-        for(int i = n-1; i >= 0; i--){
-            if(i == n-1)
-                digits[i]++;
-            if(digits[i] == 10){
+        int n = digits.size() - 1;
+        digits[n]++;
+        for (int i = n; i >= 0; i--) {
+            if (digits[i] == 10 && i-1 < 0) {
                 digits[i] = 0;
-                if(i != 0){
-                    digits[i-1]++;
-                }
-                else{
-                    digits.push_back(0);
-                    digits[i] = 1;
-                }
+                digits.insert(digits.begin(), 1);
+            }
+            if (digits[i] == 10) {
+                digits[i-1]++;
+                digits[i] = 0;
             }
         }
         return digits;
