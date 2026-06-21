e6453811dd0ba3e1826619525d2eabf8a7d78e19
William Sukjaem <133365639+Willsu00@users.noreply.github.com>
2026-06-21 16:23:49 +1200

    Time: 3 ms (5.74%) | Memory: 11.7 MB (26.67%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -2,16 +2,12 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
         int n = digits.size() - 1;
-        // 9 9 
         digits[n]++;
-
-        // 9,10
         for (int i = n; i >= 0; i--) {
             if (digits[i] == 10 && i-1 < 0) {
                 digits[i] = 0;
                 digits.insert(digits.begin(), 1);
             }
-
             if (digits[i] == 10) {
                 digits[i-1]++;
                 digits[i] = 0;
