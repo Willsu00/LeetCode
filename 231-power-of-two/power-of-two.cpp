3aa41364a0feabc3ec1e3fefe5abbeaaa302bcd6
William Sukjaem <133365639+Willsu00@users.noreply.github.com>
2026-06-24 22:33:02 +1200

    Time: 0 ms (100.00%) | Memory: 7.9 MB (11.87%) - LeetSync

---
+++ 231-power-of-two/power-of-two.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if (n <= 0) {
+            return false;
+        }
+
+        while (n % 2 == 0) {
+            n = n/2;
+        }
+
+        return n==1;
+
+    }
+};
