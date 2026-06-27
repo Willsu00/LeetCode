d689089224ed057ad838606a50094d8fdc30e445
William Sukjaem <133365639+Willsu00@users.noreply.github.com>
2026-06-27 19:54:53 +1200

    Time: 71 ms (74.81%) | Memory: 98.8 MB (33.37%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -0,0 +1,23 @@
+#include <unordered_map>
+
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int, int> map;
+
+        for (int i = 0; i < nums.size(); i++) {
+            // if nums[i] is found in map AND iterator is NOT .end()
+            // .end() is special iterator type that declares the end of the map (not last element but past the last element)
+            if(map.find(nums[i]) != map.end()) {
+                if(i - map[nums[i]] <= k) {
+                    return true;
+                }
+            }
+
+            map[nums[i]] = i;
+        }
+
+        return false;
+
+    }
+};
